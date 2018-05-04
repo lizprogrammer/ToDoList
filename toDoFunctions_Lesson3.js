5495daae52f190991e08e9bf9244eddf3a9bd959
lizprogrammer <lizprogrammer@yahoo.com>
2018-05-03 20:03:37 -0400

    Todo Version 4

--- toDoFunctions_Lesson3.js
+++ toDoFunctions_Lesson3.js
@@ -1,23 +1,53 @@
 
-var myComputer = {
-    operatingSystem: 'mac',
-    screenSize: '15 inches',
-    purchaseYear: 2011
-};
-
-console.log(myComputer);
-
-console.log(myComputer.operatingSystem);
-
-console.log(myComputer.screenSize);
-
-console.log(myComputer.purchaseYear);
+///*
+//var myComputer = {
+//    operatingSystem: 'mac',
+//    screenSize: '15 inches',
+//    purchaseYear: 2011
+//};
+//
+//console.log(myComputer);
+//
+//console.log(myComputer.operatingSystem);
+//
+//console.log(myComputer.screenSize);
+//
+//console.log(myComputer.purchaseYear);
+//*/
 
 var elizabeth = {
     name: 'Elizabeth',
     sayName: function(){
-        console.log(this);
+        console.log(this.name);
     }
 }
 
 elizabeth.sayName();
+
+var todoList = {
+    todos: ['item 1', 'item 2', 'item 3'],
+
+      displayTodos: function() {
+        console.log('My Todos', this.todos);
+      },
+
+      addTodo: function(todo) {
+          this.todos.push(todo);
+          this.displayTodos();
+        },
+        changeTodo: function(position, newValue) {
+          this.todos[position] = newValue;
+          this.displayTodos();
+        },
+
+        deleteTodo: function(position) {
+          this.todos.splice(position, 1);
+          this.displayTodos();
+        }
+
+
+    };
+
+    todoList.displayTodos();
+    console.log("hey");
+    todoList.displayTodos();
---
+++ toDoFunctions_Lesson4.js
@@ -0,0 +1,26 @@
+var todoList = {
+    todos: [],
+    displayTodos: function(){
+        console.log('My Todos', this.todos);
+
+    },
+    addTodo: function(todoText){
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+    changeTodo: function(position, todoText){
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    delteTodo: function(position){
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position){
+        var todo = this.todos[position];
+        
+    },
+}
