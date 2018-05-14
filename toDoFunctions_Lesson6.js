ad520ed202b64050a27b667835486ebca5eb078c
lizprogrammer <lizprogrammer@yahoo.com>
2018-05-13 21:46:21 -0400

    Lesson 6

--- toDoFunctions_Lesson6.js
+++ toDoFunctions_Lesson6.js
@@ -1,11 +1,11 @@
 for(var i = 0; i < 3; i ++){
-    console.log('hey');
+    //console.log('hey');
 
 
 }
 
 for(var i = 0; i < 3; i ++){
-    console.log(i);
+    //console.log(i);
 
 
 }
@@ -15,15 +15,30 @@ testArray.push('extra item');
 //console.log(testArray[1]);
 //console.log(testArray[2]);
 for(var i = 0; i < 3; i ++){
-    console.log(testArray[i]);
+    //console.log(testArray[i]);
 }
 
 var todoList = {
     todos:[],
     displayTodos: function(){
-        console.log('My Todos');
-        for(var i = 0; i < this.todos.length; i++){
-        console.log(this.todos[i].todoText);
+        if (this.todos.length === 0){
+            console.log("Your todo list is empty");
+        }
+        else{
+            console.log('My todos');
+            for(var i = 0; i < this.todos.length; i++)
+            {
+                //check if .completed is true
+                if(this.todos[i].completed == true){
+                    console.log('(x) ' , this.todos[i].todoText);
+                }
+                else{
+                    console.log('( ) ' , this.todos[i].todoText);
+                }
+                // if print with (x)
+                //else print with ( )
+
+            }
         }
     },
     addTodo: function(todoText){
@@ -31,15 +46,75 @@ var todoList = {
             todoText: todoText,
             completed: false
         });
-        this.displayTodos();
+        //this.displayTodos();
     },
 
     changeTodo: function(position, todoText){
         this.todos[position].todoText = todoText;
-        this.displayTodos();
+        //this.displayTodos();
 
+    },
+        toggleCompleted: function(position){
+        var todo = this.todos[position];
+        console.log(todo);
+        todo.completed = !todo.completed;
+        //this.displayTodos();
+    },
+
+    toggleAll: function(){
+        var totalTodos = this.todos.length;;
+        var completedTodos = 0;
+
+        // Get number of completed todos
+        for(var i = 0; i < totalTodos; i++){
+            if(this.todos[i].completed == true)
+                completedTodos++;
+        }
+
+        // If everything's true, make everything false
+        if(completedTodos == totalTodos){
+            // make everything false
+            for (var i = 0; i < totalTodos; i++)
+            {
+                this.todos[i].completed = false;
+            }
+        }
     }
-
-
 }
+
+todoList.displayTodos();
+
+todoList.addTodo('first');
+todoList.addTodo('second');
+todoList.addTodo('third');
+
+
+todoList.toggleCompleted(0);
+todoList.toggleCompleted(1);
+todoList.toggleCompleted(2);
+
+
+todoList.displayTodos();
+
+todoList.toggleAll();
+todoList.displayTodos();
+
+
+
+// if there are no todos
+// if this.todos.length is equal to 0
+// if this.todos.length === 0
+// print your todo list is empty
+
+
+
+
+
+
+
+
+
+
+
+
 
