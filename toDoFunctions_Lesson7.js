b5973e25d317821299a6b7508e2629bc1d93cefe
lizprogrammer <lizprogrammer@yahoo.com>
2018-05-13 22:05:33 -0400

    Lesson 6

---
+++ toDoFunctions_Lesson7.js
@@ -0,0 +1,32 @@
+//JavaScript data types
+
+//Objects
+//-- todoList, arrays, functions
+//Primitives (Building Blocks)
+//-- String
+//-- Number
+//-- Boolean
+//-- Undefined
+//-- Null
+
+
+// String
+var myString = "Hello! I'm a string!";
+console.log(myString === "Hello! I'm a string!")
+
+// Number
+console.log(1 === 1);
+
+// Boolean
+console.log(true == true);
+
+// Undefined
+var noValue;
+var notDefined;
+console.log(noValue);
+console.log(noValue === notDefined);
+
+// Null
+myNull = null;
+console.log(myNull);
+console.log(myNull === null);
