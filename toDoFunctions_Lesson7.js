2f7c504c8e622bf58336ab47121eb64a57f23019
lizprogrammer <lizprogrammer@yahoo.com>
2018-05-14 18:14:31 -0400

    Lesson 7

--- toDoFunctions_Lesson7.js
+++ toDoFunctions_Lesson7.js
@@ -29,3 +29,66 @@ console.log(noValue === notDefined);
 myNull = null;
 console.log(myNull);
 console.log(myNull === null);
+
+// these look alike but are not the same
+var myCompare = null;
+myCompare = ({} == {});
+console.log(myCompare);
+
+
+var myPrimitive = 10;
+
+var myObject =
+ {
+    name: 'gordon'
+}
+
+console.log(myPrimitive, ' : my primitive');
+console.log(myObject, ' : my object');
+
+
+// houses
+var myHouse = {color: 'blue'};
+console.log(myHouse.color);
+console.log(myHouse);
+
+
+myHouse.color = 'red';
+console.log(myHouse.color);
+console.log(myHouse);
+
+
+var myHouse1 = {color: 'blue'};
+var myHouse2 = myHouse1;
+
+myHouse2.color = 'red';
+
+console.log(myHouse1);
+console.log(myHouse2);
+
+// these objects are equal because they point to the same location in memory
+console.log(myHouse1 == myHouse2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
